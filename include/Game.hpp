58226817181d8958932b200735bb4089831849a2
Pavlo Schutskiy <pavlo050706@gmail.com>
2026-01-06 16:19:23 +0200

    Basic uesr input handling for movement

--- include/Game.hpp
+++ include/Game.hpp
@@ -12,7 +12,14 @@ private:
 	void update();
 	void render();
 
+	void handlePlayerInput(sf::Keyboard::Scan scan, bool isPressed);
+
 private:
 	sf::RenderWindow m_window;
 	sf::CircleShape m_player;
+
+	bool m_isMovingUp = false;
+	bool m_isMovingDown = false;
+	bool m_isMovingRight = false;
+	bool m_isMovingLeft = false;
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -10,7 +10,7 @@ Game::Game()
 }
 
 void Game::run() {
-	while (m_window.isOpen()) {
+	while (m_window.isOpen()) { // main (game) loop
 		processEvents();
 		update();
 		render();
@@ -22,15 +22,44 @@ void Game::processEvents() {
 		if (event->is<sf::Event::Closed>()) {
 			m_window.close();
 		}
+		else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+			handlePlayerInput(keyPressed->scancode, true);
+		}
+		else if (const auto* keyReleased = event->getIf<sf::Event::KeyReleased>()) {
+			handlePlayerInput(keyReleased->scancode, false);
+		}
 	}
 }
 
 void Game::update() {
+	sf::Vector2f movement({ 0.f, 0.f });
+	
+	if (m_isMovingUp)
+		movement.y -= 1.f;
+	if (m_isMovingDown)
+		movement.y += 1.f;
+	if (m_isMovingLeft)
+		movement.x -= 1.f;
+	if (m_isMovingRight)
+		movement.x += 1.f;
 
+	m_player.move(movement);
 }
 
 void Game::render() {
 	m_window.clear();
 	m_window.draw(m_player);
 	m_window.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Scan scan, bool isPressed)
+{
+	if (scan == sf::Keyboard::Scancode::W)
+		m_isMovingUp = isPressed;
+	else if (scan == sf::Keyboard::Scancode::S)
+		m_isMovingDown = isPressed;
+	else if (scan == sf::Keyboard::Scancode::D)
+		m_isMovingRight = isPressed;
+	else if (scan == sf::Keyboard::Scancode::A)
+		m_isMovingLeft = isPressed;
 }
