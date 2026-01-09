63c10bfb64a0ac13de6493b902e8325eeedd371c
Pavlo Schutskiy <pavlo050706@gmail.com>
2026-01-09 07:05:28 +0200

    Basic error handling

--- src/TextureHolder.cpp
+++ src/TextureHolder.cpp
@@ -1,21 +1,30 @@
+#include <cassert>
+#include <stdexcept>
+
 #include "TextureHolder.hpp"
 
 void TextureHolder::load(Texture id, const std::string& filename)
 {
 	std::unique_ptr<sf::Texture> texture(new sf::Texture());
 	if (!texture->loadFromFile(filename))
-		;
-	m_textureMap.insert(std::make_pair(id, std::move(texture)));
+		throw std::runtime_error("TextureHolder::load - Failed to load " + filename);
+	
+	auto inserted = m_textureMap.insert(std::make_pair(id, std::move(texture)));
+	assert(inserted.second);
 }
 
 sf::Texture& TextureHolder::get(Texture id)
 {
 	auto found = m_textureMap.find(id);
+	assert(found != m_textureMap.end());
+
 	return *found->second;
 }
 
 const sf::Texture& TextureHolder::get(Texture id) const
 {
 	auto found = m_textureMap.find(id);
+	assert(found != m_textureMap.end());
+	
 	return *found->second;
 }
