76fd1c66e3c3b362206343a16b42c5e1365e581e
Pavlo Schutskiy <pavlo050706@gmail.com>
2026-01-17 10:06:23 +0200

    added SpriteNode

---
+++ include/SpriteNode.hpp
@@ -0,0 +1,13 @@
+#include "SceneNode.hpp"
+
+class SpriteNode : public SceneNode {
+public:
+	explicit SpriteNode(const sf::Texture& texture);
+	SpriteNode(const sf::Texture& texture, const sf::IntRect& rect);
+
+private:
+	virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+	sf::Sprite m_sprite;
+};
