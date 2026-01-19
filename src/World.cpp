71d8389349b170050119317435214b7ac7497df8
Pavlo Shutz <pavlo050706@gmail.com>
2026-01-19 09:47:52 +0200

    world initialization

---
+++ src/World.cpp
@@ -0,0 +1,19 @@
+#include "World.hpp"
+
+World::World(sf::RenderWindow& window)
+	: m_window(window)
+	, m_worldView(window.getDefaultView())
+	, m_textures()
+	, m_sceneGraph()
+	, m_sceneLayers()
+	, m_worldBounds({ 0.f, 0.f	}, { m_worldView.getSize().x, 2000.f })
+	, m_spawnPosition({m_worldView.getSize().x / 2.f, m_worldBounds.size.y - m_worldView.getSize().y / 2.f})
+	, m_scrollSpeed(-50.f)
+	, m_playerAircraft(nullptr)
+{
+	loadTextures();
+	buildScene();
+
+	// Prepare the view
+	m_worldView.setCenter(m_spawnPosition);
+}
